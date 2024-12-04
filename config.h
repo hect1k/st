432e173e08797c9a3b5744214df1c83eb38c1784
Nnisarg Gada <gadannisarg@gmail.com>
2024-12-05 00:53:40 +0530

    fix font size

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:size=12";
 /* Spare fonts */
 static char *font2[] = {
-	"NotoColorEmoji:pixelsize=16:antialias=true:autohint=true",
+	"NotoColorEmoji:pixelsize=12:antialias=true:autohint=true",
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
 
